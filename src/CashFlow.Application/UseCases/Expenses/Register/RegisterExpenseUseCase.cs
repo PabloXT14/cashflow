4e929ac0e9adbe47ea131ef01e4371a22ddec175
Pablo Alan <pabloxt14@gmail.com>
2026-07-02 19:09:38 -0300

    feat: validate expenses using if

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,3 +1,4 @@
+using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
@@ -7,8 +8,35 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisteredExpenseJson request)
     {
-        // TODO: Validations
+        Validate(request);
         
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisteredExpenseJson request)
+    {
+        var isTitleEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (isTitleEmpty)
+        {
+            throw new ArgumentException("Title is required.");
+        }
+     
+        var valueIsZeroOrNegative = request.Amount <= 0;
+        if (valueIsZeroOrNegative)
+        {
+            throw new ArgumentException("Value must be greater than zero.");
+        }
+
+        var isDateOnFuture = DateTime.Compare(request.Date, DateTime.UtcNow) > 0;
+        if (isDateOnFuture)
+        {
+            throw new ArgumentException("Date cannot be in the future.");
+        }
+
+        var isPaymentTypeValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if (!isPaymentTypeValid)
+        {
+            throw new ArgumentException("Invalid payment type.");
+        }
     }
 }
