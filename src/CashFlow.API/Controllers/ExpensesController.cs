ba07d9b244c2327eeeb5b38fe4693931873576cc
Pablo Alan <pabloxt14@gmail.com>
2026-07-04 16:25:34 -0300

    feat: improve error responses

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.API.Controllers;
@@ -21,11 +22,15 @@ public class ExpensesController : ControllerBase
         }
         catch (ArgumentException ex)
         {
-            return BadRequest(ex.Message);
+            var errorResponse = new ResponseErrorJson(ex.Message);
+            
+            return BadRequest(errorResponse);
         }
         catch
         {
-            return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error occurred.");
+            var errorResponse = new ResponseErrorJson("An unexpected error occurred.");
+            
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
         }
     }
 }
