2220479ee33943047976a19ec8197b43df1d8f58
Pablo Alan <pabloxt14@gmail.com>
2026-07-13 19:05:13 -0300

    feat: create a request with fake data

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,30 @@
+using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestRegisterExpenseJsonBuilder
+{
+    public RequestRegisterExpenseJson Build()
+    {
+        var faker = new Faker();
+
+        return new RequestRegisterExpenseJson
+        {
+            Title = faker.Commerce.ProductName(),
+            Description = faker.Commerce.ProductDescription(),
+            Amount = faker.Finance.Amount(min: 1, max: 1000),
+            Date = faker.Date.Past(),
+            PaymentType = faker.PickRandom<PaymentType>(),
+        };
+
+        // Other syntax
+        // return new Faker<RequestRegisterExpenseJson>()
+        //     .RuleFor(x => x.Title, f => f.Commerce.ProductName())
+        //     .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+        //     .RuleFor(x => x.Amount, f => f.Finance.Amount(min: 1, max: 1000))
+        //     .RuleFor(x => x.Date, f => f.Date.Past())
+        //     .RuleFor(x => x.PaymentType, f => f.PickRandom<PaymentType>());
+    }
+}
