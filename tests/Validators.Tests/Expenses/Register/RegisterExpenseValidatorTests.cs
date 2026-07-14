70c21f2ea30599f948ee46e53688402c1fbed068
Pablo Alan <pabloxt14@gmail.com>
2026-07-14 19:55:52 -0300

    feat: test data generated by bogus lib

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,6 +1,7 @@
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
+using CommonTestUtilities.Requests;
 
 namespace Validators.Tests.Expenses.Register;
 
@@ -12,14 +13,7 @@ public class RegisterExpenseValidatorTests
         // Arrange (config the instances that we need to execute our test)
         var validator = new RegisterExpenseValidator();
 
-        var request = new RequestRegisterExpenseJson
-        {
-            Title = "Test expense",
-            Description = "Test description",
-            Amount = 100,
-            Date = DateTime.Now.AddDays(-1),
-            PaymentType = PaymentType.CreditCard,
-        };
+        var request = RequestRegisterExpenseJsonBuilder.Build();
 
         // Act (execute the method that we want to test)
         var result = validator.Validate(request);
