b23b37fe8e58271ed7f861e62ba447088ff48eb2
Pablo Alan <pabloxt14@gmail.com>
2026-07-16 18:03:50 -0300

    feat: create test for an exception

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using Shouldly;
 
@@ -19,5 +20,25 @@ public class RegisterExpenseValidatorTests
 
         // Assert (verify that the result is what we expected)
         result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        // Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        request.Title = string.Empty;
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            errors => errors.Count.ShouldBe(1),
+            errors => errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED))
+        );
     }
 }
