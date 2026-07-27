9de11713b5aa4a720d6da8de7bca629a2ff8c837
Pablo Alan <pabloxt14@gmail.com>
2026-07-27 18:16:27 -0300

    feat: register db-context as DI

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Infrastructure.DataAccess.Repositories;
+using CashFlow.Infrastructure.DataAccess;
 
 namespace CashFlow.Infrastructure;
 
@@ -9,6 +10,17 @@ public static class DependencyInjectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection services)
     {
+        AddDbContext(services);
+        AddRepositories(services);
+    }
+
+    public static void AddRepositories(IServiceCollection services)
+    {
         services.AddScoped<IExpensesRepository, ExpensesRepository>();
+    }
+
+    public static void AddDbContext(IServiceCollection services)
+    {
+        services.AddDbContext<CashFlowDbContext>();
     }
 }
