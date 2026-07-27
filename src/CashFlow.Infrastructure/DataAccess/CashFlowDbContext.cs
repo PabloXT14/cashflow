6c7fc494f6f7026eec3cbcdd4c2de251ca421083
Pablo Alan <pabloxt14@gmail.com>
2026-07-27 18:33:39 -0300

    feat: use appsettings for connection string

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -5,16 +5,10 @@ namespace CashFlow.Infrastructure.DataAccess;
 
 internal class CashFlowDbContext : DbContext
 {
+    public CashFlowDbContext(DbContextOptions options) : base(options) { }
+
     // OBS: O nome da propriedade DbSet deve ser o mesmo nome da tabela no banco de dados (não precisa ser case-sensitive), caso contrário, será necessário configurar o mapeamento no OnModelCreating.
     public DbSet<Expense> Expenses { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var connectionString = "Server=localhost;Port=3306;Database=cashflow_db;User=cashflow_user;Password=@Password123;";
 
-        var version = new Version(8, 4);
-        var serverVersion = new MySqlServerVersion(version);
-
-        optionsBuilder.UseMySql(connectionString, serverVersion);
-    }
 }
