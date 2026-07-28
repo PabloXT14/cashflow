c6d6db4311e0fe8d08162a49fec4ff9af20859e2
Pablo Alan <pabloxt14@gmail.com>
2026-07-28 18:48:54 -0300

    feat: add Tasks to our code

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -18,7 +18,7 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
         _unitOfWork = unitOfWork;
     }
 
-    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    public async Task<ResponseRegisteredExpenseJson> Execute(RequestRegisterExpenseJson request)
     {
         Validate(request);
 
@@ -31,8 +31,8 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
             PaymentType = (Domain.Enums.PaymentType)request.PaymentType,
         };
 
-        _expensesRepository.Add(entity);
-        _unitOfWork.Commit();
+        await _expensesRepository.Add(entity);
+        await _unitOfWork.Commit();
 
         return new ResponseRegisteredExpenseJson
         {
