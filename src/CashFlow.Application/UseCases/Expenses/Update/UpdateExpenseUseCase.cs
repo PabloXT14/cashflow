3549dd761601f64a41259d904360369974a5e362
Pablo Alan <pabloxt14@gmail.com>
2026-08-02 18:56:13 -0300

    feat: update expense (part 2)

--- src/CashFlow.Application/UseCases/Expenses/Update/UpdateExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Update/UpdateExpenseUseCase.cs
@@ -3,6 +3,7 @@ using CashFlow.Communication.Requests;
 using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.Update;
@@ -23,6 +24,19 @@ public class UpdateExpenseUseCase : IUpdateExpenseUseCase
     public async Task Execute(long id, RequestExpenseJson request)
     {
         Validate(request);
+
+        var expense = await _expenseRepository.GetById(id);
+
+        if (expense is null)
+        {
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+        }
+
+        _mapper.Map(request, expense);
+
+        _expenseRepository.Update(expense);
+
+        await _unitOfWork.Commit();
     }
 
     private void Validate(RequestExpenseJson request)
