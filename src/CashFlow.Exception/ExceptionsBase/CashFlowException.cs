a845e88d64e00804250e34d90be430aa7fef95f3
Pablo Alan <pabloxt14@gmail.com>
2026-08-02 17:15:57 -0300

    feat: apply O from SOLID on our project

--- src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
+++ src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
@@ -2,6 +2,9 @@ namespace CashFlow.Exception.ExceptionsBase;
 
 public abstract class CashFlowException : SystemException
 {
+    public abstract int StatusCode { get; }
+    public abstract List<string> GetErrors();
+
     protected CashFlowException(string message) : base(message)
     {
     }
