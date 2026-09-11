90f7c9dab2a0b2ed64ae1fb8991f226aa0de2a01
Pablo Alan <pabloxt14@gmail.com>
2026-09-11 17:47:28 -0300

    feat: create our first migration

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -8,7 +8,8 @@ internal class CashFlowDbContext : DbContext
     public CashFlowDbContext(DbContextOptions options) : base(options) { }
 
     // OBS: O nome da propriedade DbSet deve ser o mesmo nome da tabela no banco de dados (não precisa ser case-sensitive), caso contrário, será necessário configurar o mapeamento no OnModelCreating.
-    public DbSet<Expense> Expenses { get; set; }
 
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<User> Users { get; set; }
 
 }
